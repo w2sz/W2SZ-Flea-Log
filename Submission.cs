c198c7027f85efd7e0f2404d0092032c8b98114a
AliasKugelblitz <prashansadoo@gmail.com>
2026-02-04 18:07:25 -0500

    Changed ignore to allow publish folder

--- Submission.cs
+++ Submission.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace W2SZ_Flea_Log
+﻿namespace W2SZ_Flea_Log
 {
     internal class Submission
     {
